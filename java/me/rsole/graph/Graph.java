194ae0131361bf8c818bd0f16754f65138568943
Ricard Solé <ricardsolecasas@gmail.com>
2017-07-16 22:25:49 +0100

    graph building scaffolding

--- java/me/rsole/graph/Graph.java
+++ java/me/rsole/graph/Graph.java
@@ -10,15 +10,28 @@ public class Graph {
     this.vertices = new HashMap<>();
   }
 
-  void addVertex(String name) {
+  public void addVertex(String name) {
     vertices.putIfAbsent(name, new Vertex(name));
   }
 
-  void addEdge(Vertex from, Vertex to) {
-    vertices.get(from.getKey()).addNeighbor(to);
+  public void addEdge(String from, String to) {
+    Vertex fromV = vertices.get(from);
+    Vertex toV = vertices.get(to);
+
+    if (fromV == null) {
+      addVertex(from);
+      fromV = getVertex(from);
+    }
+
+    if (toV == null) {
+      addVertex(to);
+      toV = getVertex(to);
+    }
+
+    fromV.addNeighbor(toV);
   }
 
-  void addBiEdge(Vertex a, Vertex b) {
+  public void addBiEdge(String a, String b) {
     addEdge(a, b);
     addEdge(b, a);
   }
--- java/me/rsole/ladder/Ladder.java
+++ java/me/rsole/ladder/Ladder.java
@@ -1,9 +1,62 @@
 package me.rsole.ladder;
 
+import com.google.common.collect.Sets;
 import me.rsole.graph.Graph;
 
-class Ladder {
-  public static void main(String[] args) {
-    Graph g = new Graph();
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.*;
+
+public class Ladder {
+  private Ladder() {}
+
+  public static void main(String[] args) throws FileNotFoundException {
+    Graph g = buildGraph(groupWords(getWords()));
+  }
+
+  private static Graph buildGraph(Map<String, Set<String>> groups) {
+    Graph graph = new Graph();
+
+    for (Map.Entry<String, Set<String>> g : groups.entrySet()) {
+      if (g.getValue().size() <= 1) {
+        continue;
+      }
+
+      for (List<String> product : Sets.cartesianProduct(g.getValue(), g.getValue())) {
+        if (product.get(0).equals(product.get(1))) {
+          continue;
+        }
+
+        graph.addBiEdge(product.get(0), product.get(1));
+      }
+    }
+
+    return graph;
+  }
+
+  private static Map<String, Set<String>> groupWords(Set<String> words) {
+    Map<String, Set<String>> groups = new HashMap<>();
+
+    for (String w : words) {
+      for (int i = 0; i < w.length(); i++) {
+        String groupName = String.format("%s_%s", w.substring(0, i), w.substring(i + 1));
+        groups.putIfAbsent(groupName, new HashSet<>());
+        groups.get(groupName).add(w);
+      }
+    }
+
+    return groups;
+  }
+
+  private static Set<String> getWords() throws FileNotFoundException {
+    File f = new File("third_party/data/words.txt");
+    Scanner s = new Scanner(f);
+    Set<String> words = new HashSet<>();
+
+    while (s.hasNextLine()) {
+      words.add(s.nextLine());
+    }
+
+    return words;
   }
 }
